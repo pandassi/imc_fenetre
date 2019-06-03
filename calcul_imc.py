4673aa4be6ea124d36132e128adf4405ff805415
Pandassi <47264697+pandassi@users.noreply.github.com>
2019-06-03 19:12:58 -0400

    Update calcul_imc.py

--- calcul_imc.py
+++ calcul_imc.py
@@ -1,5 +1,5 @@
 from mod_bmi_modele import Bmi
 
 
-def calcul_imc(unpatient: Bmi): #voit pas Bmi donc l'importer (copier coller)
+def calcul_imc(unpatient: Bmi): #voit pas BMI donc l'importer (copier coller)
     return unpatient.poids / (unpatient.taille **2) #reste à importer dans l'autre fenêtre
